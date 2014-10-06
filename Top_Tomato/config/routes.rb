812cd041f5e2a027e8f6761d28f5805b291f0214
Elwend <elizabeth.wendling@gmail.com>
2014-10-05 22:33:11 -0400

    Update routes, added to application.yml

--- Top_Tomato/config/routes.rb
+++ Top_Tomato/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :user_movies
 
-  resources :comments
+  resources :movies do
+    resources :comments
 
-  resources :movies
-
-  devise_for :users
+  root 'movies#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
